ac344bfcab21bb171c2057a0cf09f4e594363ae8
nemocrk <nemocrk@users.noreply.github.com>
2026-01-02 23:17:10 +0100

    chore: register JsonExceptionMiddleware in settings

--- backend/wedding/settings.py
+++ backend/wedding/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.JsonExceptionMiddleware',  # Custom Error Handler for API
 ]
 
 ROOT_URLCONF = 'wedding.urls'
