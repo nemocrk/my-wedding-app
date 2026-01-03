cd8a53b37de0bbb17ae022065769ba80238d81f5
nemocrk <nemocrk@users.noreply.github.com>
2026-01-03 10:11:50 +0100

    Register AccommodationViewSet in urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import InvitationViewSet
+from .views import InvitationViewSet, AccommodationViewSet
 
 router = DefaultRouter()
 router.register(r'invitations', InvitationViewSet)
+router.register(r'accommodations', AccommodationViewSet, basename='accommodation')
 
 urlpatterns = [
     path('', include(router.urls)),
