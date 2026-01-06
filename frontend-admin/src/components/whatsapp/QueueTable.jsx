4608fbeda1892b7440029aed27de40c544b29a35
nemocrk <nemocrk@users.noreply.github.com>
2026-01-06 19:29:02 +0100

    Remove overflow-x from table wrapper

--- frontend-admin/src/components/whatsapp/QueueTable.jsx
+++ frontend-admin/src/components/whatsapp/QueueTable.jsx
@@ -61,7 +61,7 @@ const QueueTable = ({ messages, realtimeStatus, onRetry, onForceSend, onDelete, 
 
   return (
     <div className="flex flex-col relative">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div className="-my-2 sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
