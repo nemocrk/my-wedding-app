154473a5df2e8a298b84735a3cbb6ce530d6e85d
nemocrk <nemocrk@users.noreply.github.com>
2026-01-14 13:08:57 +0100

    feat(frontend-user): wrap App with TextProvider
    Added TextProvider to root component to make configurable texts available throughout the app.
    Completes FASE 3.2.

--- frontend-user/src/main.jsx
+++ frontend-user/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n' // Inizializza i18n
 import App from './App.jsx'
+import { TextProvider } from './contexts/TextContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TextProvider>
+      <App />
+    </TextProvider>
   </StrictMode>,
 )
